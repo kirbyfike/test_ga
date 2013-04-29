6b6ad0c248f4ed9c91800f749ea6e253cd0457b3
Kirby Fike <kirbyfike@gmail.com>
2013-04-29 16:45:26 -0700

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TestGa::Application.routes.draw do
+  
+  match '/signup',  to: 'organizations#new',            via: 'get'
+
   #get "pages/home"
   match '/about', to: 'pages#about', via: 'get'
   match '/contact', to: 'pages#contact', via: 'get'
