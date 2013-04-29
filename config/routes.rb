16f5f198f96b755aac296f00c6acf1248cebd939
Kirby Fike <kirbyfike@gmail.com>
2013-04-29 15:38:05 -0700

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 TestGa::Application.routes.draw do
-  get "pages/home"
+  #get "pages/home"
+  match '/about', to: 'pages#about', via: 'get'
+  match '/contact', to: 'pages#contact', via: 'get'
 
   root to: 'pages#home'
 end
